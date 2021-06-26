e744796b60790c3979574468014f6e1c1e5e741e
flintco <connor.flint16@gmail.com>
2021-06-25 20:00:39 -0500

    Added ListNode code

--- Arrays/Single-Linked-List/src/ListNode.java
+++ Arrays/Single-Linked-List/src/ListNode.java
@@ -1,6 +1,12 @@
 
 public class ListNode {
 	//Variables: Data, Pointer to next node
+	int data;
+	ListNode next;
 	
 	//Constructor for ListNode(data)
+	public ListNode(int value) {
+		this.data = value;
+		this.next = null;
+	}
 }
